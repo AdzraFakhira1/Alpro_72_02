ba3b770c9943caee046f99d939959396b289cb6f
Adzra Fakhira <Adzrafakhira2@gmail.com>
2020-05-22 20:12:41 +0700

    Tugas Algoritma Pemprograman

---
+++ src/ch02/ObjectPersegi.java
@@ -0,0 +1,8 @@
+package ch02;
+public class ObjectPersegi {
+    public static void main(String[] args) {
+        Persegi x = new Persegi();
+        x.sisi= 2;
+        System.out.println("Luas persegi = " + x.luas());
+    }
+}
